99fa65b23f89bafc1a6f9505f7abb79f200b85cb
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-07-13 15:10:28 -0500

    updated code.js; tests.js

--- tests.js
+++ tests.js
@@ -16,6 +16,13 @@ describe('helloWorld', function() {
 /////
 describe('sayHello',function() {
     it("should be a defined function", function() {
-        expect(typeof sayHello).toBe('function')
+        expect(typeof sayHello).toBe('function');
+    });
+    it("should return a string when called", function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
     });
 });
+
